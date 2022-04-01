d19faf69bd7d6f65c3e442331435a275f3cbea61
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2022-04-01 22:02:01 +0900

    2022-0323

--- brute-force/11723_집합.js
+++ brute-force/11723_집합.js
@@ -39,26 +39,26 @@ const readFileSyncAddress = 'input.txt';
 
 const fs = require('fs');
 const input = fs.readFileSync(readFileSyncAddress).toString().split('\n');
+//console.log(input);
 
 //답안작성 
 
-const m = +input[0];
+const m = +(input.shift());
 const arr = [];
-
-for(let i = 0; i <= m; i++){
-    arr.push(input[i].replace(/\n/g, "").split(' '));
-    if(!isNaN(arr[i][1])) arr[i][1] = +arr[i][1];
-}
-arr.shift();
 
 let S = 0; 
 let answer = '';
 
 for(let i = 0; i < m; i++){
+    arr.push(input[i].replace(/\n/g, "").split(' '));
+    if(!isNaN(arr[i][1])) arr[i][1] = +arr[i][1];
+
+    //n값이 undefined일 때 코드 동작 X, 임의로 넣었다.
+    if(arr[i].length === 1) arr[i].push(0);
     let cmd = arr[i][0];
     let n = arr[i][1];
     bitOperator(cmd,n);
-    console.log(i,cmd,n,S.toString(2));
+    //console.log(i,cmd,n,S.toString(2));
 }
 
 function bitOperator(cmd,n){
@@ -77,10 +77,14 @@ function bitOperator(cmd,n){
             S ^= (1<<n);
             break;
         case 'all' :
-            S = ((1<<20)-1);
+            S = (1<<21)-1;
             break;
         case 'empty' :
             S = 0;
             break;
+
     }
 }
+
+//제출
+console.log(answer);

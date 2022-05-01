98041bf6123b2490125b136bdb8a126a365eeeb0
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2022-05-01 16:54:26 +0900

    2022-0501

---
+++ 06.문자열 단계/10809.js
@@ -0,0 +1,48 @@
+/*input
+5
+54321
+*/
+
+// 백준 제출용
+// const readFileSyncAddress = '/dev/stdin';
+
+// VSC 테스트용
+const readFileSyncAddress = 'input.txt';
+
+const fs = require('fs');
+let S = fs.readFileSync(readFileSyncAddress).toString().trim();
+
+const alphabets = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+let answer = '';
+for (let alphabet of alphabets) {
+  answer += `${S.indexOf(alphabet)} `;
+}
+console.log(answer);

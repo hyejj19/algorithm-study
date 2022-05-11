ded6bda895627555c0973d8d3c16ad6d3aa3b4fb
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2022-05-11 20:27:04 +0900

    Update 2941.js

--- 06.문자열 단계/2941.js
+++ 06.문자열 단계/2941.js
@@ -12,12 +12,20 @@ const fs = require('fs');
 let str = fs.readFileSync(readFileSyncAddress).toString().trim();
 
 const croatian = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+
 const solution = function (str) {
-  let cnt = 0;
-  for (const x of croatian) {
-    let pos = 
-  }
-  console.log(str);
+  for (let x of croatian) str = str.replaceAll(x, 'x');
+
+  return str.length;
 };
 
-solution(str);
+console.log(solution(str));
+
+// for (let i = 0; i < str.length; i++) {
+//   for (let j = 0; j < croatian.length; j++) {
+//     if (str.includes(croatian[i])) {
+//       str = str - str.substring(str.indexOf(croatian[i]), croatian.length);
+//       cnt++;
+//     }
+//   }
+// }
